5035954275f08c098ff44c12facac7b0616631de
mrmrs <hi@mrmrs.cc>
2018-07-19 17:35:09 +0100

    Custom styles

--- examples/html.js
+++ examples/html.js
@@ -1,18 +1,27 @@
-// example template
 module.exports = ({
-  html,
-  css,
+  html = '',
+  css = '',
   scripts,
-  title,
+  js,
+  publicPath,
+  title = 'x0',
   meta = [],
   links = [],
-  static: isStatic
-}) => `<!DOCTYPE html>
+  static: staticBuild
+}) =>
+`<!DOCTYPE html>
 <head>
-  <title>{title}</title>
-  ${css}
+<meta charset='utf-8'>
+<meta name='viewport' content='width=device-width,initial-scale=1'>
+<meta name='generator' content='Compositor x0'>
+<title>${title}</title>
+${meta.map(({ name, content }) => `<meta name='${name}' content='${content}'>`).join('\n')}
+${links.map(({ rel, href }) => `<link rel='${rel}' href='${href}' />`).join('\n')}
+<style>body { margin :0; }</style>
+${css}
 </head>
+<body>
 <div id=root>${html}</div>
-${scripts}
+${staticBuild ? '' : scripts}
+</body>
 `
-
